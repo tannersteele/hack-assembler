276751e6c4279fa4c8dbb02700fb69cfb1502ae4
Tanner Steele <tannerjsteele@gmail.com>
2024-03-30 17:59:06 -0500

    initial project commit

---
+++ HackAssembler/HackAssembler.cpp
@@ -0,0 +1,12 @@
+﻿// HackAssembler.cpp : Defines the entry point for the application.
+//
+
+#include "HackAssembler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hack Assembler entrypoint" << endl;
+	return 0;
+}
